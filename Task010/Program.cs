f3b2dded8d6c0a99ce8e432957ecca28e552873c
Pavel <pmaskarev@vk.com>
2023-03-28 10:16:50 +0300

    Added Task10

---
+++ Task010/Program.cs
@@ -0,0 +1,33 @@
+﻿Main();
+
+void Main()
+{
+    bool Work = true;
+    while (Work)
+    {
+        System.Console.WriteLine("Решаем задачу? Введите y/n.");
+        string a = Console.ReadLine();
+        switch (a)
+        {
+            case "y":task(); break;
+            case "n": Work = false; break;
+        }
+    }
+}
+
+void task()
+{
+    System.Console.WriteLine("Введите трёхзначное число:");
+    int a = int.Parse(Console.ReadLine());
+    int itog = WhatIsTheSecond(a);
+    System.Console.WriteLine($"Вторая цифра числа - {itog}");
+}
+
+int WhatIsTheSecond(int number)
+{
+    int a = number%100;
+    int b = number%10;
+    number = ( a - b)/10;
+    return number;
+
+}
