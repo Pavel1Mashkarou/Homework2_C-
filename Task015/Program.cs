8b15f1cbd7a3d580f62e590690694df476e11342
Pavel <pmaskarev@vk.com>
2023-03-28 11:28:38 +0300

    added task 015

---
+++ Task015/Program.cs
@@ -0,0 +1,36 @@
+﻿Main();
+
+void Main()
+{
+    bool Work = true;
+    while (Work)
+    {
+        System.Console.WriteLine("Решаем задачу? Введите y/n.");
+        string a = Console.ReadLine();
+        switch(a)
+        {
+            case "y": Osn();break;
+            case "n": Work = false; break;
+        }
+    }
+}
+
+void Osn()
+{
+    System.Console.WriteLine("Введите номер дня:");
+    int a = int.Parse(Console.ReadLine());
+    int res = Check(a);
+    if (res == 0) System.Console.WriteLine("День рабочий");
+    if (res == 2) System.Console.WriteLine("День выходной");
+    if (res == -1) System.Console.WriteLine("Нет такого дня в неделе. В неделе всего 7 дней.");
+
+}
+
+int Check(int number)
+{
+    int a = 0;
+    if (number<6 && number>0) a = 0;
+    if (number<0 || number>7) a = -1;
+    if (number==6 || number== 7) a = 2;
+    return a;
+}
